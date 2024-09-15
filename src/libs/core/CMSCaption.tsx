1b459c05cae979113a27505ad3d2edf58b3da701
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-15 20:02:36 +0200

    Add src/libs/core/CMSCaption.tsx

---
+++ src/libs/core/CMSCaption.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+import { theme, Theme } from '../theme';
+
+const useStyles = createUseStyles<string, {}, any>((theme: Theme) => ({
+  text: {
+    fontFamily: 'Montserrat, sans-serif',
+    color: 'var(--black)',
+    fontWeight: 300,
+    fontSize: 12,
+    lineHeight: 1.5,
+    margin: 0,
+    width: 'fit-content',
+    borderBottom: 'none',
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderTop: 'none',
+    '&:hover': {},
+  },
+  '@container (max-width: 768px)': { text: {} },
+  '@container (max-width: 480px)': { text: {} },
+}));
+
+interface Props {
+  text: string;
+}
+
+type TextProps = React.HTMLAttributes<HTMLParagraphElement>;
+
+export const CMSCaption = ({ text, ...rest }: TextProps & Props) => {
+  const classes = useStyles({ theme });
+
+  return (
+    <p className={classes.text} {...rest}>
+      {text}
+    </p>
+  );
+};
