5ee86827bb7fb6c59f26ee860a1240c84d233d62
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-15 20:02:38 +0200

    Add src/libs/core/CMSBlock.tsx

---
+++ src/libs/core/CMSBlock.tsx
@@ -0,0 +1,39 @@
+import React, { useMemo } from 'react';
+import { createUseStyles } from 'react-jss';
+import { theme, Theme } from '../theme';
+
+const useStyles = createUseStyles<string, {}, any>((theme: Theme) => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '100%',
+    gap: 32,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: 'auto',
+    background: 'var(--blueLight)',
+    padding: 16,
+    borderRadius: 8,
+    '&:hover': {},
+  },
+  '@container (max-width: 768px)': { container: {} },
+  '@container (max-width: 480px)': { container: {} },
+}));
+
+type BlockProps = React.HTMLAttributes<HTMLDivElement>;
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export const CMSBlock = (props: BlockProps & Props) => {
+  const { children, ...rest } = props;
+  const classes = useStyles({ theme });
+
+  return (
+    <div className={classes.container} {...rest}>
+      {children}
+    </div>
+  );
+};
