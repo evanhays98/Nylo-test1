3de44b2220f1caae0d176bc82352503f706916ab
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-15 20:02:42 +0200

    Add src/pages/NewPage.tsx

---
+++ src/pages/NewPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CMSPage, CMSText1, CMSLabel, CMSBody } from 'src/libs/core';
+export const NewPage = () => {
+  return (
+    <CMSPage>
+      <CMSText1 text="Create & Customize Your Dream Components"></CMSText1>
+
+      <CMSLabel text="Unleash your creativity! Easily add, create, and style components to bring your ideas to life."></CMSLabel>
+
+      <CMSBody text="Start designing your unique creations now!"></CMSBody>
+    </CMSPage>
+  );
+};
