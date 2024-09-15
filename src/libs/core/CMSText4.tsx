704155a356f684df9f0e80334c0230381dda0f00
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-15 20:02:34 +0200

    Add src/libs/core/CMSText4.tsx

---
+++ src/libs/core/CMSText4.tsx
@@ -0,0 +1,39 @@
+import React, { forwardRef, useMemo } from 'react';
+import { createUseStyles } from 'react-jss';
+import { theme, Theme } from '../theme';
+import { CMSPage } from './CMSPage';
+
+const useStyles = createUseStyles<string, {}, any>((theme: Theme) => ({
+  text: {
+    fontFamily: 'Montserrat, sans-serif',
+    color: 'var(--black)',
+    fontWeight: 800,
+    fontSize: 18,
+    lineHeight: 1.5,
+    margin: 0,
+    width: 'fit-content',
+    borderBottom: 'none',
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderTop: 'none',
+    '&:hover': {},
+  },
+  '@container (max-width: 768px)': { text: {} },
+  '@container (max-width: 480px)': { text: {} },
+}));
+
+interface Props {
+  text: string;
+}
+
+type TextProps = React.HTMLAttributes<HTMLHeadingElement>;
+
+export const CMSText4 = ({ text, ...rest }: TextProps & Props) => {
+  const classes = useStyles({ theme });
+
+  return (
+    <h4 className={classes.text} {...rest}>
+      {text}
+    </h4>
+  );
+};
