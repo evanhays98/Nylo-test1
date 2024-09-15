06639a1ef4cdf8386c56b2f62a0f6ac6932b6b7f
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-15 21:07:14 +0200

    Update src/pages/FirstPage.tsx

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,112 +1,13 @@
 import React from 'react';
-import {
-  CMSPage,
-  CMSText10,
-  CMSLabel,
-  CMSBlock,
-  CMSText2,
-  CMSText4,
-} from 'src/libs/core';
+import { CMSPage, CMSText1, CMSLabel, CMSBody } from 'src/libs/core';
 export const FirstPage = () => {
   return (
     <CMSPage>
-      <CMSText10 text="Create & Customize Your Dream Components"></CMSText10>
+      <CMSText1 text="Create & Customize Your Dream Components"></CMSText1>
 
       <CMSLabel text="Unleash your creativity! Easily add, create, and style components to bring your ideas to life."></CMSLabel>
 
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-
-        <CMSText4 text="New Text"></CMSText4>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
-
-      <CMSBlock>
-        <CMSText2 text="New Text"></CMSText2>
-
-        <CMSText4 text="New Text"></CMSText4>
-      </CMSBlock>
+      <CMSBody text="Start designing your unique creations now!"></CMSBody>
     </CMSPage>
   );
 };
