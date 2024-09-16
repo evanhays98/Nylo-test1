694fd7c41790ce7d8e76aef69cd4fad161927d76
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-16 22:22:38 +0200

    Add src/libs/core/CMSLabel0.tsx

---
+++ src/libs/core/CMSLabel0.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+import { theme, Theme } from '../theme';
+
+const useStyles = createUseStyles<string, {}, any>((theme: Theme) => ({
+  text: {
+    fontFamily: 'Montserrat, sans-serif',
+    color: theme.colors.green12,
+    fontWeight: 700,
+    fontSize: 14,
+    lineHeight: 1.5,
+    margin: 0,
+    width: 'fit-content',
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderTop: 'none',
+    borderRadius: 0,
+    '&:hover': {},
+  },
+  '@media (max-width: 768px)': { text: {} },
+  '@media (max-width: 480px)': { text: {} },
+}));
+
+interface Props {
+  text: string;
+}
+
+type TextProps = React.HTMLAttributes<HTMLParagraphElement>;
+
+export const CMSLabel0 = ({ text, ...rest }: TextProps & Props) => {
+  const classes = useStyles({ theme });
+
+  return (
+    <p className={classes.text} {...rest}>
+      {text}
+    </p>
+  );
+};
