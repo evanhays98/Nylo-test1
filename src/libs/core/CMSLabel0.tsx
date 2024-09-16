1fd3d68018010467c62b5886899fd505246d242d
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-16 22:29:05 +0200

    Update src/libs/core/CMSLabel0.tsx

--- src/libs/core/CMSLabel0.tsx
+++ src/libs/core/CMSLabel0.tsx
@@ -11,6 +11,7 @@ const useStyles = createUseStyles<string, {}, any>((theme: Theme) => ({
     lineHeight: 1.5,
     margin: 0,
     width: 'fit-content',
+    borderBottom: 'none',
     borderLeft: 'none',
     borderRight: 'none',
     borderTop: 'none',
