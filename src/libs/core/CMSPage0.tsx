4b207ad7623d19023dbe713266fadbe2e28e318f
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-16 22:18:18 +0200

    Add src/libs/core/CMSPage0.tsx

---
+++ src/libs/core/CMSPage0.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+import { theme, Theme } from '../theme';
+
+const useStyles = createUseStyles<string, {}, any>((theme: Theme) => ({
+  container: {
+    minHeight: '100vh',
+    display: 'flex',
+    width: '100%',
+    height: 'auto',
+    gap: 16,
+    flexDirection: 'column',
+    background: theme.colors.green1,
+    padding: 16,
+    borderRadius: 0,
+    '&:hover': {},
+  },
+  '@media (max-width: 768px)': { container: {} },
+  '@media (max-width: 480px)': { container: {} },
+}));
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+type BlockProps = React.HTMLAttributes<HTMLDivElement>;
+
+export const CMSPage0 = ({ children, ...rest }: BlockProps & Props) => {
+  const classes = useStyles({ theme });
+
+  return (
+    <div className={classes.container} {...rest}>
+      {children}
+    </div>
+  );
+};
