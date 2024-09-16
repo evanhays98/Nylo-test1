71445266c2b44215b2518e6ad02faa737f467f2c
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-16 20:03:12 +0200

    Update src/libs/core/CMSBody.tsx

--- src/libs/core/CMSBody.tsx
+++ src/libs/core/CMSBody.tsx
@@ -5,7 +5,7 @@ import { theme, Theme } from '../theme';
 const useStyles = createUseStyles<string, {}, any>((theme: Theme) => ({
   text: {
     fontFamily: 'Montserrat, sans-serif',
-    color: 'var(--black)',
+    color: '{theme.colors.black}',
     fontWeight: 400,
     fontSize: 13,
     lineHeight: 1.5,
