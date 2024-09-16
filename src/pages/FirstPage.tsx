c8802468f8a034498de9891854941bfe795162d2
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-16 22:18:21 +0200

    Update src/pages/FirstPage.tsx

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { CMSPage, CMSText1, CMSLabel, CMSBody } from 'src/libs/core';
+import { CMSPage0, CMSText1, CMSLabel, CMSBody } from 'src/libs/core';
 export const FirstPage = () => {
   return (
-    <CMSPage>
+    <CMSPage0>
       <CMSText1 text="Create & Customize Your Dream Components"></CMSText1>
 
       <CMSLabel text="Unleash your creativity! Easily add, create, and style components to bring your ideas to life."></CMSLabel>
 
       <CMSBody text="Start designing your unique creations now!"></CMSBody>
-    </CMSPage>
+    </CMSPage0>
   );
 };
