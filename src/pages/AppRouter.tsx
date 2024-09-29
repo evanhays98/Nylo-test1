809d17b4d43f8d10272cc8a63bbe50fc93c7f403
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-29 16:30:13 +0200

    Update src/pages/AppRouter.tsx

--- src/pages/AppRouter.tsx
+++ src/pages/AppRouter.tsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 
-import { FirstPage, NewPage } from 'src/pages';
+import { FirstPage } from 'src/pages';
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<FirstPage />} />
-      <Route path="/newpage" element={<NewPage />} />
     </Routes>
   );
 };
