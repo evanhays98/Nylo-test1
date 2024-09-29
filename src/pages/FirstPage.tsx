02fab4fd87c5c123a95dc35978ac3ca26e753a76
evanhays98 <51299115+evanhays98@users.noreply.github.com>
2024-09-29 16:30:11 +0200

    Update src/pages/FirstPage.tsx

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,51 +1,13 @@
 import React from 'react';
-import {
-  CMSPage0,
-  CMSText1,
-  CMSLabel,
-  CMSBody,
-  CMSBlock0,
-  CMSBlock6,
-  CMSLabel4,
-  CMSBlock10,
-  CMSLabel2,
-  CMSBlock1,
-  CMSLabel3,
-  CMSBlock4,
-  CMSLabel0,
-  CMSBlock7,
-  CMSLabel1,
-} from 'src/libs/core';
+import { CMSPage, CMSText1, CMSLabel, CMSBody } from 'src/libs/core';
 export const FirstPage = () => {
   return (
-    <CMSPage0>
+    <CMSPage>
       <CMSText1 text="Create & Customize Your Dream Components"></CMSText1>
 
       <CMSLabel text="Unleash your creativity! Easily add, create, and style components to bring your ideas to life."></CMSLabel>
 
       <CMSBody text="Start designing your unique creations now!"></CMSBody>
-
-      <CMSBlock0>
-        <CMSBlock6>
-          <CMSLabel4 text="Diana"></CMSLabel4>
-        </CMSBlock6>
-
-        <CMSBlock10>
-          <CMSLabel2 text="Diana"></CMSLabel2>
-        </CMSBlock10>
-
-        <CMSBlock1>
-          <CMSLabel3 text="Diana"></CMSLabel3>
-        </CMSBlock1>
-
-        <CMSBlock4>
-          <CMSLabel0 text="Diana"></CMSLabel0>
-        </CMSBlock4>
-
-        <CMSBlock7>
-          <CMSLabel1 text="Diana"></CMSLabel1>
-        </CMSBlock7>
-      </CMSBlock0>
-    </CMSPage0>
+    </CMSPage>
   );
 };
